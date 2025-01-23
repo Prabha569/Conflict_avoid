640a43c46c412184dc2e9836f0aeb20ae06d61bd
PRABHAKAR NAYAK <prabhakarnayak993@gmail.com>
2025-01-23 11:49:32 +0530

    update a.txt, student.txt

--- student.java
+++ student.java
@@ -1,6 +1,11 @@
 class Student{
   int id=101;
   String name="prabha";
+<<<<<<< Updated upstream
   String addrs="chatrapur";
   Boolean bo="true";
+=======
+>>>>>>> Stashed changes
+  Double d=5000f;
+  String addrs="Ganjam";
 }
