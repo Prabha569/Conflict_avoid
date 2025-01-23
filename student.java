b4a1664981a254a877021c26f6ad0b37fe9f55c3
PRABHAKAR NAYAK <prabhakarnayak993@gmail.com>
2025-01-23 12:06:34 +0530

    Revert "update a.txt, student.txt"
    This reverts commit 640a43c46c412184dc2e9836f0aeb20ae06d61bd.

--- student.java
+++ student.java
@@ -1,11 +1,6 @@
 class Student{
   int id=101;
   String name="prabha";
-<<<<<<< Updated upstream
   String addrs="chatrapur";
   Boolean bo="true";
-=======
->>>>>>> Stashed changes
-  Double d=5000f;
-  String addrs="Ganjam";
 }
